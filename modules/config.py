70ec634af507497c75564ca71c6b3686bd86bba9
patriotic07 <duniyatg0@gmail.com>
2025-02-12 00:09:52 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -2,6 +2,6 @@
 import os
 BOT_NAME = os.environ.get('zenith')
 BOT_TOKEN = os.environ.get('7781303244:AAFQbzJM80oa_sMGDzJDGnrDxxa9965Uh50')
-API_ID = int(os.environ.get('25503802'))
+API_ID = int(os.environ.get("25503802"))
 API_HASH = os.environ.get('c55b9f84a3833e4363c644862d3b3fb6')
 MONGO_URI = os.environ.get('mongodb+srv://sahilkoyeb:n8tDyCMEKhHyh8ms@cluster0.edzie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
